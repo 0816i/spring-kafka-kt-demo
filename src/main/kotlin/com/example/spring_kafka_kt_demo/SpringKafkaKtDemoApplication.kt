d43b9892c912992eaed4401a1e285e7ea2df9502
0816i <0816i@naver.com>
2025-02-17 10:47:18 +0900

    feat: init

---
+++ src/main/kotlin/com/example/spring_kafka_kt_demo/SpringKafkaKtDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spring_kafka_kt_demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKafkaKtDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringKafkaKtDemoApplication>(*args)
+}
